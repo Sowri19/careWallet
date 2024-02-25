018b5dae6d9458760f91c7fc7fbbcbb647ffa4fd
Satya Sowri Sampath Korturti <98078922+Sowri19@users.noreply.github.com>
2024-02-24 21:47:20 -0500

    Add ChatBot component and styles, and Home component and styles

---
+++ modules/Home/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyledImage } from './styles';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <StyledImage
+        src="/public/assets/carewallet.png"
+        alt="Description of the image"
+      />
+    </div>
+  );
+};
+
+export default Home;
